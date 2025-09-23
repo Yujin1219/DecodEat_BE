107c7c928ab0fa8caf451766bfda29025f7dbf5f
junho <2171168@hansung.ac.kr>
2025-09-23 13:27:48 +0900

    feat: 상품 기반 추천

--- src/main/java/com/DecodEat/domain/products/client/PythonAnalysisClient.java
+++ src/main/java/com/DecodEat/domain/products/client/PythonAnalysisClient.java
@@ -1,7 +1,9 @@
 package com.DecodEat.domain.products.client;
 
 import com.DecodEat.domain.products.dto.request.AnalysisRequestDto;
+import com.DecodEat.domain.products.dto.request.ProductBasedRecommendationRequestDto;
 import com.DecodEat.domain.products.dto.response.AnalysisResponseDto;
+import com.DecodEat.domain.products.dto.response.ProductBasedRecommendationResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,5 +34,19 @@ public class PythonAnalysisClient {
                 .timeout(Duration.ofMinutes(2))
                 .doOnSuccess(response -> log.info("Analysis completed with status: {}", response.getDecodeStatus()))
                 .doOnError(error -> log.error("Analysis request failed: {}", error.getMessage()));
+    }
+
+    public Mono<ProductBasedRecommendationResponseDto> getProductBasedRecommendation(
+    ProductBasedRecommendationRequestDto request){
+        log.info("Sending analysis request to Python server: {}", pythonServerUrl);
+
+        return webClient.post()
+                .uri(pythonServerUrl + "api/v1/recommend/product-based")
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(ProductBasedRecommendationResponseDto.class)
+                .timeout(Duration.ofMinutes(2))
+                .doOnSuccess(response -> log.info("Success to get recommendation(product-based): {}",request.getProduct_id()))
+                .doOnError(error -> log.error("Recommendation request failed: {}", error.getMessage()));
     }
 }
